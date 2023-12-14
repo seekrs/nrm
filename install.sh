c46f50505eca8dc384c120352ebeeda73ec1277e
xtrm <oss@xtrm.me>
2023-12-14 03:53:49 +0100

    🐛 fix(install.sh): ok im dumb

--- install.sh
+++ install.sh
@@ -7,17 +7,18 @@ python3 -m pip install -Ur requirements.txt --break-system-packages
 chmod +x ./nrm
 
 # Create local bin directory 
-mkdir -p $USER/.local/bin
+mkdir -p $HOME/.local/bin
 
 # Symlink the executable to the local bin directory
-ln -s ./nrm $USER/.local/bin/nrm
+ln -s ./nrm $HOME/.local/bin/nrm
 
 # Add the local bin directory to the PATH
 SHELL=$(echo $SHELL | xargs basename)
-SHELLRC=".bashrc"
+SHELLRC="$HOME/.bashrc"
 if [ $SHELL == "zsh" ]; then
-	SHELLRC=".zshrc"
+	SHELLRC="$HOME/.zshrc"
 elif [ $SHELL == "fish" ]; then
-	SHELLRC=".config/fish/config.fish"
+	XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+	SHELLRC="$XDG_CONFIG_HOME/fish/config.fish"
 fi
-echo "export PATH=$USER/.local/bin:$PATH" >> ~/$SHELLRC
+echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> $SHELLRC
