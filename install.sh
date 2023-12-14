385a5eff91223d1ae1d3ced128eb552fe9dbe428
xtrm <oss@xtrm.me>
2023-12-14 03:59:20 +0100

    🐛 fix(install.sh): better error handling

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Install the dependencies
-python3 -m pip install -Ur requirements.txt --break-system-packages || echo "Failed to install the dependencies" && exit 1
+echo "Installing python3 dependencies"
+python3 -m pip install -Ur requirements.txt --break-system-packages >/dev/null
 
-# Make executable
+echo "Make file executable"
 chmod +x ./nrm
 
-# Create local bin directory 
+echo "Create local bin directory" 
 mkdir -p $HOME/.local/bin
 
-# Symlink the executable to the local bin directory
+echo "Symlink the executable to the local bin directory"
 cp ./nrm $HOME/.local/bin/nrm || ln -s $(pwd)/nrm $HOME/.local/bin/nrm || echo "Failed to copy or symlink the executable to the local bin directory" && exit 1
 
-# Add the local bin directory to the PATH
+echo "Add the local bin directory to the PATH"
 SHELL=$(echo $SHELL | xargs basename)
 SHELLRC="$HOME/.bashrc"
 if [ $SHELL == "zsh" ]; then
@@ -21,4 +21,5 @@ elif [ $SHELL == "fish" ]; then
 	XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 	SHELLRC="$XDG_CONFIG_HOME/fish/config.fish"
 fi
+echo "Adding to $SHELLRC"
 echo "export PATH=\"\$HOME/.local/bin:\$PATH\"" >> $SHELLRC
